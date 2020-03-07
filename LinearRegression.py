749fb1f4b6420323a3b466b041cf0d10dcc2a582
hulubaby <455879568@qq.com>
2020-03-07 12:57:25 +0800

    'optimize format'

--- LinearRegression.py
+++ LinearRegression.py
@@ -62,15 +62,15 @@ def gradient_descent(points, starting_w, starting_b, lr, iterations):
     """
     w, b = starting_w, starting_b
 
-    for iter in range(iterations):
+    for item in range(iterations):
         # 对所有的数据根据均方误差更新一次w和b
         w, b = step_gradient(w, b, np.array(points), lr)
         # 计算当前的均方误差，用于监控训练进度
         loss = mse(w, b, points)
         train_result.append([loss, w, b])
         # 对所有数据训练100次打印一次训练结果
-        if iter%50 == 0:
-            print(f"Iteration: {iter}, loss: {loss}, w: {w}, b: {b}")
+        if item % 50 == 0:
+            print(f"Iteration: {item}, loss: {loss}, w: {w}, b: {b}")
 
     # 返回最后的w和b
     return [w, b]
@@ -90,7 +90,7 @@ for i in range(100):
     # 给样本添加均值为0，方差为0.1×0.1的高斯噪声
     eps = np.random.normal(0., 0.1)
     # 获取样本标签
-    y = 1.5 * x + 0.1 + eps # 故意让样本点在直线两侧分布
+    y = 1.5 * x + 0.1 + eps  # 故意让样本点在直线两侧分布
     # 保存样本数据
     data.append([x, y])
 # 将样本数据转换为numpy数组
@@ -111,6 +111,6 @@ print(f'Best result epoch: {best_index + 1}, loss: {best_loss}, w: {best_w}, b: 
 
 
 # 绘制loss
-X = np.linspace(0,1000,1000,endpoint=True)
+X = np.linspace(0, 1000, 1000, endpoint=True)
 plt.plot(X, loss_all)
 plt.show()
